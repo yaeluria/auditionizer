f4180fc1bd71dbd1a87e2dee7e2cc9907dd5cc8e
Yael Luria <yaeluria@gmail.com>
2020-05-16 21:08:45 +0300

    extract Footer to seperate component

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link'
+
+function Footer() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://yaeluria.github.io/">
+            Yael Luria
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+        );
+  }
+export default Footer;
