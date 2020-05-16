a1ae51a05312b3e5fd8710232de5f5fc598da325
Yael Luria <yaeluria@gmail.com>
2020-05-16 21:16:34 +0300

    remove unsused styles and components

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,114 +1,68 @@
-import React, { useRef, useEffect, useState} from 'react';
+import React from 'react';
 import { observer } from 'mobx-react-lite';
-import PieceList from './PieceList';
 import PieceListContainer from './PieceListContainer';
 import NotLoggedInScreen from './NotLoggedInScreen';
-import SelectDialog from './SelectDialog';
 import SignInDialog from './SignInDialog';
-import CreateListDialog from './CreateListDialog';
-import ChooseListDialog from './ChooseListDialog';
-
-import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-
-
+import Footer from './Footer';
 import { useAppStore } from '../useAppStore';
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-       Yael Luria
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
 
 const useStyles = makeStyles(theme => ({
-  '@global': {
-    body: {
-      backgroundColor: theme.palette.common.white,
-    },
-  },
-  paper: {
-    marginTop: theme.spacing(8),
+  root:{
     display: 'flex',
     flexDirection: 'column',
-    alignItems: 'center',
+    height: '100vh'
   },
- 
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-  
   toolbar: {
     flexWrap: 'wrap',
   },
   toolbarTitle: {
     flexGrow: 1,
   },
-  login:{
-    margin: theme.spacing(1, 1.5),
+  content:{
+    flex: '1 0 auto',
+    color: 'red'
+  },
+  footer:{
+    marginTop: 'auto',
+    flexShrink: 0
   }
 }));
 
 const Main = observer(() => {
   const AppStore = useAppStore();
   const classes = useStyles();
-
-  let textInput = useRef(null);
-  
-  const addPiece = (evt) => {
-    evt.preventDefault();
-    AppStore.add(textInput.current.value);
-    if(AppStore.pieceFieldError.length < 0){
-      textInput.current.value = '';
-    }
-  };
-
- const createList = "create a list";
- const chooseList = "Choose from my lists"
- const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-
   return (
-      <div>
-      <AppBar position="static" color="default" elevation={0} className={classes.appBar}>
-      <Toolbar className={classes.toolbar}>
-        <Typography variant="h6" color="inherit" noWrap className={classes.toolbarTitle}>
-        AUDITIONIZER
+    <div className="classes.root">
+      <CssBaseline />
+      <div className="classes.content">
+        <AppBar position="static" color="default"  >
+          <Toolbar className={classes.toolbar}>
+            <Typography variant="h6" color="inherit" noWrap className={classes.toolbarTitle}>
+              AUDITIONIZER
         </Typography>
-        <SignInDialog />
-      </Toolbar>
-    </AppBar>
-    <div>
-      {AppStore.loggedIn?
- <PieceListContainer /> :
- <NotLoggedInScreen />
-  }  
-    </div> 
- 
+            <SignInDialog />
+          </Toolbar>
+        </AppBar>
+       <div>
+        {AppStore.loggedIn ?
+            <PieceListContainer /> :
+            <NotLoggedInScreen />
+          } 
+        </div> 
       </div>
-      
-   
+      <footer className="classes.footer">
+        <Container>
+          <Footer />
+        </Container>
+      </footer>
+    </div>
   );
 })
 export default Main;
