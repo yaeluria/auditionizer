ada404eb6419cc86f1e2c506d5c05ab522623348
Yael Luria <yaeluria@gmail.com>
2020-05-22 11:46:36 +0300

    add useState, fix delete by mistake from last commit

--- src/components/CreateListDialog.js
+++ src/components/CreateListDialog.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {toJS} from 'mobx';
 import { observer } from 'mobx-react-lite';
 import Button from '@material-ui/core/Button'
